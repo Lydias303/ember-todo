87b4e844228f7f556b93b0823bfba107173a0e50
Lydias303 <shaferlydia@yahoo.com>
2015-05-04 17:45:36 -0600

    added app.import to bootstrap in the brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
-('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
   destDir: 'fonts'
 });
